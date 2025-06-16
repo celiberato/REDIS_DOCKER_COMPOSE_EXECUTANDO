5e4cf8f30e28460213d2a3107facc1d91bdaee47
Carlos <celiberato@hotmail.com>
2025-06-16 18:45:47 -0300

    refactdin

--- src/main/java/com/example/demo/RedisExample.java
+++ src/main/java/com/example/demo/RedisExample.java
@@ -8,7 +8,7 @@ public class RedisExample {
 	
     public void execute(int i) {
         Jedis jedis = new Jedis("localhost", 6379); // Endereço e porta do Redis
-        jedis.set(CHAVE, "============>" + System.currentTimeMillis());
+        jedis.set(CHAVE, "============>	" + System.currentTimeMillis());
         
         System.out.println(">>>>> VALOR ENVIADO: " + System.currentTimeMillis());
         System.out.println(">>>>> VALOR RECEBIDO: " + jedis.get(CHAVE));
