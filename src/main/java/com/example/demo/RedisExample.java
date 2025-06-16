6f48d22b2616a2adc61d0fa012f0a7e5b8576ca4
Carlos <celiberato@hotmail.com>
2025-06-16 18:35:02 -0300

    refactdin

--- src/main/java/com/example/demo/RedisExample.java
+++ src/main/java/com/example/demo/RedisExample.java
@@ -10,7 +10,7 @@ public class RedisExample {
         Jedis jedis = new Jedis("localhost", 6379); // Endereço e porta do Redis
         jedis.set(CHAVE, "VALOR ARMAZENAMENTO CHAVE X VALOR - " + "{id: " + i + ", nome: JOSÉ, CPF: 2092934848}");
         
-        String resultado = jedis.get(CHAVE).split("-")[0];
+        String resultado = jedis.get(CHAVE).split("-")[1];
         
         System.out.println(">>>>> VALOR RECEBIDO: " + resultado);
         
